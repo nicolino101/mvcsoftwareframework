e80edc36d0649025ce0d3dde85a54f42d2916265
Nick <nick@mvcsoftware.com>
2012-12-02 02:19:50 -0800

    na

--- library/Mvcs/Request/IRequest.php
+++ library/Mvcs/Request/IRequest.php
@@ -2,5 +2,21 @@
 
 interface IRequest
 {
+	public function getRequest();
 	
+	public function getModule();
+	
+	public function setController($key);
+	
+	public function getController();
+	
+	public function setAction($key);
+	
+	public function getAction();
+	
+	public function setParam($key, $val);
+	
+	public function getParams();
+	
+	public function getParam($key);
 }
