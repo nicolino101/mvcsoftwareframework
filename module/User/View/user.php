b4e8dc392ca3aeb82cd0d0c9396e377533ca90ef
Nick <nick@mvcsoftware.com>
2012-12-03 12:01:27 -0800

    added js and css to view

--- module/User/View/user.php
+++ module/User/View/user.php
@@ -1,3 +1,15 @@
+<html>
+
+<head>
+
+<?php echo $this->jsFiles; ?>
+
+<?php echo $this->cssFiles; ?>
+
+</head>
+
+<body>
+
 <div class="leftside">
 
 <h1><?php echo $this->title; ?></h1>
@@ -9,7 +21,11 @@
 // $this->result[0]->getFirstname();
 // $this->result[0]->getAddress()->getCity();
 
+// this uses the view helper
 \User\View\ViewHelper::showAssociations($this);
 ?>
 
 </div>
+
+</body>
+</html>
